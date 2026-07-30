4796a84ef32a652e4c03f252b3fbf68fb621e898
lahiru-chandipa <lahiruchandipa44@gmail.com>
2026-07-31 03:33:53 +0530

    Update DishCard component to enhance availability message and improve layout

--- campuseats-dashboard/src/features/menu/components/DishCard.jsx
+++ campuseats-dashboard/src/features/menu/components/DishCard.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 function DishCard({ dish }) {
  return (
   <div className="dish-card">
     <h3>{dish.name}</h3>
     <p>Rs. {dish.price.toFixed(2)}</p>
     <small>{dish.category}</small>
-    {!dish.available && <span> — Sold out</span>}
+    {!dish.available && <span>Sold out</span>}
+    <br/><br/>
+    <Link className="dish-link" to={`/dish/${dish.id}`}>
+      Show Details
+    </Link>
   </div>
  );
 }
